0230c2833e80844e29e5609c27d3e4df3cee9ed0
Jayark <Tsume@users.noreply.github.com>
2020-02-25 10:51:34 -0600

    removed obsolete files

--- resources/deploy.x64.cmd
+++
@@ -1,8 +0,0 @@
-SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC
-
-MKDIR x64
-
-copy ..\bin\x64\release\MapleSeed.exe x64\MapleSeed.exe
-::copy ..\bin\x64\release\MapleSeed.pdb x64\MapleSeed.pdb
-
-D:\Projects\Libraries\Qt\5.12.5\msvc2017_64\bin\windeployqt --no-translations --no-opengl-sw -no-angle --plugindir x64\plugins x64\MapleSeed.exe
--- resources/deploy.x86.cmd
+++
@@ -1,8 +0,0 @@
-SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC
-
-MKDIR x86
-
-copy ..\bin\x86\release\MapleSeed.exe x86\MapleSeed.exe
-::copy ..\bin\x86\release\MapleSeed.pdb x86\MapleSeed.pdb
-
-D:\Projects\Libraries\Qt\5.12.5\msvc2017\bin\windeployqt --no-translations --no-opengl-sw --no-angle --plugindir x86\plugins x86\MapleSeed.exe
